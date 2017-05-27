036a7211e3ae80cf131fc36afa797e6712394e6b
Junsik94 <2318ws@naver.com>
2017-05-27 16:45:51 +0900

    도움말최초

---
+++ 도움말1차.c
@@ -0,0 +1,11 @@
+void help(void)
+{
+    int input_char;
+    input_char = getch();
+    if(input_char =='d'){
+      system("clear");
+      printf("명령내용\n");
+      input_char = getch();
+      if(input_char =='d')
+                  return;
+}
