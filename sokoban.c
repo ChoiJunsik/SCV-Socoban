ccb02a74885a1028c816eadee8f99cdb39ba23a3
park-se-jun <qkrtpwns1999@naver.com>
2017-05-28 12:53:06 +0900

    (커맨드) 버그 수정, 이동 알고리즘 수정(이상현)

--- sokoban.c
+++ sokoban.c
@@ -5,13 +5,13 @@
 #define WIDTH 30
 #define HEIGHT 30
 #define STAGE 5
-char name[10];
+char name[10]={0};
 char map[STAGE][WIDTH][HEIGHT];
-int move(int);
+void move(char,int);
 int scan_at_x(int);
 int scan_at_y(int);
 int getch(void);
-int scan_at_y(int i);
+int scan_at_y(int);
 char key_input(int);
 void start(void);
 
@@ -19,42 +19,180 @@ int main(void)
 {
 	  char ch=0;
 	  start();
-	  for(int i=0;i<=4;i++){
+		for(int x;x<=30;x++)
+			for(int stage=0;stage<=4;stage++){
 	      while(1){
-		  printf("hello %s",name);
-		  for(int y=0; y<WIDTH;y++){
-			 for(int x=0; x<HEIGHT;x++)
-				printf("%c",map[i][y][x]);} 
-	    	  printf("(Command)");
-               	  key_input(i);
-		  printf("\n");
-      	          system("clear"); 
-       	      }      
+		      printf("hello %s",name);
+		      for(int y=0; y<WIDTH;y++){
+			       for(int x=0; x<HEIGHT;x++)
+				  printf("%c",map[stage][y][x]);}
+	        printf("(Command)");
+          key_input(stage);
+          printf("\n");
+          system("clear");
+		    }
   }
   return 0;
 }
-int scan_at_x(int i)
+int scan_at_x(int stage)
 {
 	int x,y=0;
 	for(;y<=30;y++){
 		for(;x<=30;x++)
 		{
-			if (map[i][y][x]=='@')
-			return y;
+			if (map[stage][y][x]=='@')
+			return x;
 		}
 	}
 }
-int scan_at_y(int i)
+int scan_at_y(int stage)
 {
 	int x,y=0;
 	for(;y<=30;y++){
 		for(;x<=30;x++)
 		{
-			if (map[i][y][x]=='@')
+			if (map[stage][y][x]=='@')
 			return y;
 		}
 	}
 }
+void move (char key,int i){//i==스테이지
+    int x = scan_at_x(i);
+    int y = scan_at_y(i);
+    switch (key){
+
+        case 'j' :
+
+            if (map[i][y+1][x] == '#') //다음칸이 벽
+                return;
+            else if (map[i][y+1][x] == ' '){ //다음칸이 공간
+                map[i][y][x] = ' ';
+                map[i][y+1][x] = '@';
+                    return;
+            }
+            else if (map[i][y+1][x] == 'O'){ //다음칸이 은행
+                map[i][y][x] = ' ';
+                map[i][y+1][x] = '@';
+                    return;
+            }
+            else if (map[i][y+1][x] == '$'){ //다음칸이 돈
+                if (map[i][y+2][x] == '#' || map[i][y+2][x] == '$') //다다음칸이 벽이나 돈
+                    return;
+                else if (map[i][y+2][x] == 'O'){ //다다음칸이 은행
+                    map[i][y][x] = ' ';
+                    map[i][y+1][x] = '@';
+                    map[i][y+2][x] = '$';
+                    return;
+                }
+    else if (map[i][y+2][x] == ' '){ //다다음칸이 공간
+                    map[i][y][x] = ' ';
+                    map[i][y+1][x] = '@';
+                    map[i][y+2][x] = '$';
+                    return;
+                }
+            }
+            return;
+
+        case 'k' :
+
+            if (map[i][y-1][x] == '#') //다음칸이 벽
+                return;
+            else if (map[i][y-1][x] == ' '){ //다음칸이 공간
+                map[i][y-1][x] = '@';
+                map[i][y][x] = ' ';
+                    return;
+            }
+            else if (map[i][y-1][x] == 'O'){ //다음칸이 은행
+                map[i][y-1][x] = '@';
+                map[i][y][x] = ' ';
+                    return;
+            }
+            else if (map[i][y-1][x] == '$'){ //다음칸이 돈
+                if (map[i][y-2][x] == '#' || map[i][y-2][x] == '$') //다다음칸이 벽이나 돈
+                    return;
+                else if (map[i][y-2][x] == 'O'){ //다다음칸이 은행
+                    map[i][y-2][x] = '$';
+                    map[i][y-1][x] = '@';
+                    map[i][y][x] = ' ';
+                    return;
+                }
+    else if (map[i][y-2][x] == ' '){ //다다음칸이 공간
+                    map[i][y-2][x] = '$';
+                    map[i][y-1][x] = '@';
+                    map[i][y][x] = ' ';
+                    return;
+                }
+            }
+            break;
+
+        case 'h' :
+
+            if (map[i][y][x-1] == '#')
+                    return;
+            else if (map[i][y][x-1] == ' '){
+                map[i][y][x-1] = '@';
+                map[i][y][x] = ' ';
+                    return;
+            }
+            else if (map[i][y][x-1] == 'O'){
+                map[i][y][x-1] = '@';
+                map[i][y][x] = ' ';
+                    return;
+            }
+ else if (map[i][y][x-1] == '$'){
+                if (map[i][y][x-2] == '#' || map[i][y][x-2] == '$')
+                    return;
+                else if (map[i][y][x-2] == 'O'){
+                    map[i][y][x-2] = '$';
+                    map[i][y][x-1] = '@';
+                    map[i][y][x] = ' ';
+                    return;
+                }
+                else if (map[i][y][x-2] == ' '){
+                    map[i][y][x-2] = '$';
+                    map[i][y][x-1] = '@';
+                    map[i][y][x] = ' ';
+                    return;
+                }
+            }
+            return;
+
+        case 'l' :
+
+            if (map[i][y][x+1] == '#')
+                    return;
+            else if (map[i][y][x+1] == ' '){
+                map[i][y][x] = ' ';
+                map[i][y][x+1] = '@';
+                    return;
+            }
+            else if (map[i][y][x+1] == 'O'){
+                map[i][y][x] = ' ';
+                map[i][y][x+1] = '@';
+                    return;
+            }
+ else if (map[i][y][x+1] == '$'){
+                if (map[i][y][x+2] == '#' || map[i][y][x+2] == '$')
+                    return;
+                else if (map[i][y][x+2] == 'O'){
+                    map[i][y][x] = ' ';
+                    map[i][y][x+1] = '@';
+                    map[i][y][x+2] = '$';
+                    return;
+                }
+                else if (map[i][y][x+2] == ' '){
+                    map[i][y][x] = ' ';
+                    map[i][y][x+1] = '@';
+                    map[i][y][x+2] = '$';
+                    return;
+                }
+            }
+            return;
+        default :
+            break;
+    }
+}
+
 int getch(void){
  int ch;
  struct termios buf;
@@ -69,14 +207,14 @@ int getch(void){
  tcsetattr(0, TCSAFLUSH, &save);
  return ch;
 }
-char key_input(int i)
+char key_input(int stage)
 {
   char key=getch();
   switch(key){
     case 'h':
     case 'j':
     case 'k':
-    case 'l':move(key); break;
+    case 'l':move(key,stage); break;
     case 'u':printf("%c",key);break;
     case 'r':printf("%c",key);break;
     case 'n':printf("%c",key);break;
@@ -86,8 +224,8 @@ char key_input(int i)
     case 'd':printf("%c",key);break;
     case 't':printf("%c",key);break;
     default :break;
-   return 0;	     
-  } 
+   return 0;
+  }
 
 }
 
